92db36c9dd766c19f49c93c85839951d4e77d995
sankalp kumar <65161219+sankalp475@users.noreply.github.com>
2022-07-30 11:32:22 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -11,23 +11,23 @@ function clock() {
 	const HH = now.getHours() > 12 ? now.getHours() - 12 : now.getHours();
 	const MM = now.getMinutes();
 	const SS = now.getSeconds();
-	const SS_degrees = ((SS / 60) * 360) + 90
-	const MM_degrees = ((MM / 60) * 360) + ((SS/60)*6) + 90
-	const HH_degrees = (((HH == 0? 12 : HH) / 12) * 360) + ((MM/60)*30) + 90;
 
+	const SS_degrees = 6 * SS;
+	const MM_degrees = 6 * MM;
+	const HH_degrees = 30 * HH + MM / 2;
 	hour.style.setProperty('--hh', `${HH_degrees}deg`)
 	min.style.setProperty('--mm', `${MM_degrees}deg`)
 	second.style.setProperty('--ss', `${SS_degrees}deg`)
 
-	_hour_.innerHTML = HH == 0? 12 : HH
-	_min_.innerHTML = MM
-	_second_.innerHTML = SS
-	const time = {
-		h : HH == 0? 12 : HH,
-		m: MM,
-		s: SS,
-	}
-    console.log(time)
+	// _hour_.innerHTML = HH == 0? 12 : HH
+	// _min_.innerHTML = MM
+	// _second_.innerHTML = SS
+	// const time = {
+	// 	h : HH == 0? 12 : HH,
+	// 	m: MM,
+	// 	s: SS,
+	// }
+    // console.log(time)
 }
 setInterval(clock,1000)
 clock()
